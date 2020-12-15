aced49e5db507491b626f4139d84e25279758e96
Tim Hockin <thockin@google.com>
2020-12-15 12:43:17 -0800

    Add logr.WithCallDepth() support

--- example/main.go
+++ example/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"github.com/go-logr/logr"
 	"github.com/go-logr/zapr"
 	"go.uber.org/zap"
 )
@@ -29,6 +30,14 @@ func (e E) Error() string {
 	return e.str
 }
 
+func Helper(log logr.Logger, msg string) {
+	helper2(log, msg)
+}
+
+func helper2(log logr.Logger, msg string) {
+	logr.WithCallDepth(log, 2).Info(msg)
+}
+
 func main() {
 	log := zapr.NewLogger(zap.NewExample())
 	log = log.WithName("MyName").WithValues("user", "you")
@@ -37,4 +46,5 @@ func main() {
 	log.V(1).V(1).Info("you should NOT see this")
 	log.Error(nil, "uh oh", "trouble", true, "reasons", []float64{0.1, 0.11, 3.14})
 	log.Error(E{"an error occurred"}, "goodbye", "code", -1)
+	Helper(log, "thru a helper")
 }
